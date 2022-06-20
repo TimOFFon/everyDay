2e9efae7cb8a501d2ffc3ce42e5fc461e9685123
Timofey <sukonkin.timofei@gmail.com>
2022-06-20 16:36:42 +0300

    V2_refactoring_CO - FINISHED!!!

--- V2_refactoring_CO.js
+++ V2_refactoring_CO.js
@@ -1,5 +1,5 @@
 function NewOrder() {
-    let locked = false;
+    
 //================================================================
 //=========================    addItem     =====================
 //================================================================
@@ -7,20 +7,20 @@ function NewOrder() {
     this.addItem = function(item, count) {
         if(this.locked) {
             this.lockOrder();
-        };
-
-        if (!this.check) {
-            this.check = {};
-        };
-
-        if (item.name in this.check && !this.locked) {
-            return this.check[item.name].count += count;
-        } else if(!this.locked) {
-            return this.check[item.name] = {
-                name: item.name,
-                cost: item.cost,
-                count: count,
-            }  
+        } else {
+            if (!this.check) {
+                this.check = {};
+            };
+    
+            if (item.name in this.check && !this.locked) {
+                return this.check[item.name].count += count;
+            } else if(!this.locked) {
+                return this.check[item.name] = {
+                    name: item.name,
+                    cost: item.cost,
+                    count: count,
+                }  
+            };
         };
     };
 //================================================================
@@ -33,18 +33,18 @@ removeItem(item, count) - убрать из чека count итемов
 */
     this.removeItem = function(item, count) {
         if(this.locked) {
-            this.lockOrder();
-        };
-
-        if(item.name in this.check&& !this.locked) {
-            if(this.count === undefined ||
-                this.count >= this.check[item.name].count) {
-                delete this.check[item.name];
-            } else {
-                this.check[item.name].count -= this.count;
+            this.reportIsLocked();
+            return;
+        } else {
+            if(item.name in this.check) {
+                if(this.count === null ||
+                    this.count >= this.check[item.name].count) {
+                    delete this.check[item.name];
+                } else {
+                    this.check[item.name].count -= count;
+                };
             };
         };
-
     };
 //================================================================
 //=========================    getCheck    =======================
@@ -57,38 +57,39 @@ removeItem(item, count) - убрать из чека count итемов
     this.getCheck = function() {
 
         if(this.locked) {
-            this.lockOrder();
+            this.reportIsLocked();
+            return;
+        } else {
+            if(this.check) {
+                let sumCountsItem = Object.keys(this.check).length;
+                let itemName = null; 
+                let costItem = null; 
+                let countItem = null; 
+                let totalCostItem = null; 
+                let totalCostOrder = null;
+                let fullStr = '';
+    
+                let firrstPartString = `*==============================================*\n Заказ состоит из ${sumCountsItem} позиций: \n`;
+                fullStr += firrstPartString;
+    
+                for(let key in this.check) {
+                    itemName = this.check[key].name;
+                    costItem = this.check[key].cost;
+                    countItem = this.check[key].count;
+                    totalCostItem = costItem * countItem;
+                    totalCostOrder += totalCostItem;
+    
+                    let middlePartsString = `*==============================================*\n ${itemName} по цене: ${costItem} руб. за 1шт.\n Количество ${itemName}: ${countItem}шт. \n На сумму: ${totalCostItem} руб.\n`;
+                    fullStr += middlePartsString;
+                };
+    
+                let lastPartString = `*==============================================*\n Общая сумма заказа: ${totalCostOrder}`;
+    
+                fullStr += lastPartString; 
+    
+                return console.log(fullStr);
+            };  
         };
-        
-        if(this.check && !this.locked) {
-            let sumCountsItem = Object.keys(this.check).length;
-            let itemName = null; 
-            let costItem = null; 
-            let countItem = null; 
-            let totalCostItem = null; 
-            let totalCostOrder = null;
-            let fullStr = '';
-
-            let firrstPartString = `*==============================================*\n Заказ состоит из ${sumCountsItem} позиций: \n`;
-            fullStr += firrstPartString;
-
-            for(let key in this.check) {
-                itemName = this.check[key].name;
-                costItem = this.check[key].cost;
-                countItem = this.check[key].count;
-                totalCostItem = costItem * countItem;
-                totalCostOrder += totalCostItem;
-
-                let middlePartsString = `*==============================================*\n ${itemName} по цене: ${costItem} руб. за 1шт.\n Количество ${itemName}: ${countItem}шт. \n На сумму: ${totalCostItem} руб.\n`;
-                fullStr += middlePartsString;
-            };
-
-            let lastPartString = `*==============================================*\n Общая сумма заказа: ${totalCostOrder}`;
-
-            fullStr += lastPartString; 
-
-            return console.log(fullStr);
-        };  
     };
 //================================================================
 //=========================    lockOrder    =====================
@@ -101,28 +102,25 @@ removeItem(item, count) - убрать из чека count итемов
     this.lockOrder = function() {
         if(this.check && !this.locked) {
             this.locked = true;
+            this.reportIsLocked();
         };
-        function showMessageBlock() {
-            let messageStr = `Заказ заблокирован!\n Снимите блокировку!`;
-            return console.log(messageStr);
-        };
-        return showMessageBlock();
+        
     };
-
 //================================================================
 //=========================    unlockOrder    =====================
 //================================================================
     this.unloсkOrder = function() {
         if(this.check && this.locked) {
-            showMessageUnBlock();
-            return  this.locked = false;
-        };
-        function showMessageUnBlock() {
-            let messageStr = `Заказ разаблокирован!\n Операции доступны`;
-            return console.log(messageStr);
-        };
+            this.locked = false;
+            this.reportIsLocked();
+        };  
         
     };
+
+
+    this.reportIsLocked = function() {
+        return this.locked === true ?  console.log(`Заказ заблокирован!\n Снимите блокировку!`) : console.log(`Заказ разаблокирован!\n Операции доступны`);
+    }
 };
 
 
@@ -140,33 +138,44 @@ let item3 = {
 };
 
 let order = new NewOrder();
-order.addItem(item, 2);
-order.addItem(item2, 2);
-order.addItem(item3, 2);
-order.removeItem(item, 1);
-console.log(order);
-order.lockOrder();
-order.getCheck();
+// order.addItem(item, 2);
+// order.addItem(item2, 2);
+// order.addItem(item3, 2);
+// order.removeItem(item, 1);
+// console.log(order);
+// order.lockOrder();
 // order.getCheck();
-order.unloсkOrder();
-order.lockOrder();
-order.getCheck();
+// // order.getCheck();
+// order.unloсkOrder();
+// order.lockOrder();
+// order.getCheck();
+// order.getCheck();
+// order.removeItem(item, 1);
+// order.unloсkOrder();
+// order.removeItem(item, 1);
+
+// order.addItem(item2, 2);
+// order.getCheck();
+// order.removeItem(item2, 1);
+// order.getCheck();
+
+
 /**
- * 40 убрать, ТК везде юзаем из this.
+ * 40 убрать, ТК везде юзаем из this.✔
  *  
- * Понять разницу this.locked и let locked
+ * Понять разницу this.locked и let locked✔
  * 
- * 46 зачем ловить если уже залоченг? Если залочен, return и все
+ * 46 зачем ловить если уже залоченг? Если залочен, return и все✔
  * 
  * 54 убрать проверку на this.locked, ТК если,
- * locked делаем return 
+ * locked делаем return ✔
  * 
  * выше LockOrder не должен юзаться только для вывода сообщения, 
  * если нужно выводить сообщение добавить метод reportIsLocked
- * Ну то есть функции на 143 158 вынести в методы
+ * Ну то есть функции на 143 158 вынести в методы✔
  * 
  * Остальное на что не указал явно но где тоже самое 
- * сделать по аналогии
+ * сделать по аналогии✔
  * 
  * Продумать требования к порядку вывода в getCheck, 
  * как должен считаться порядок. 
@@ -174,14 +183,11 @@ order.getCheck();
  * Описать требования здесь, 
  * потом уже перейти к коду. 
  * 
- * Проверить, выполняются ли требования уже, 
- * если нет доработать код. 
- * 
- * Проверить кейс с удалением, как работает в нем, 
- * если надо доработать требования, потом код 
- * 
- * Если с требованием сложности, сходить в магазин 
- * купить несколько товаров, посмотреть в каком 
- * порядке их пробьют на кассе и как они 
- * располагаются в чеке, понять закономерность
+ * Позиций: <кол>   
+ * ========================================
+ * Название товара / цена за шт
+ * полное название и артикул / кол в заказе
+ * Покупок: <кол> или <сумма стоимости позиции>
+ * =============================================
+ * ИТОГ                           = <цена>
  */
